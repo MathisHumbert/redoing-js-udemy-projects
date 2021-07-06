5d5c4dd9987ef210a0c022be3cdbc041e3d550c0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-07-06 17:38:05 +0200

    project n°1 done without help

--- Color Flipper/hex.js
+++ Color Flipper/hex.js
@@ -1,1 +1,21 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.getElementById('btn');
+const text = document.querySelector('.color');
+
+const getRandomNumber = () => {
+  let randomNumber = Math.floor(Math.random() * hex.length);
+  return randomNumber;
+};
+
+getRandomNumber();
+btn.addEventListener('click', () => {
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    let number = hex[getRandomNumber()];
+    color += number;
+  }
+  document.body.style.background = color;
+  text.textContent = color;
+  text.style.color = color;
+});
