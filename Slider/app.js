f2d6a2f639a5403cd454953c4f70e45f630acc5a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-07-11 16:31:23 +0200

    project n°12

---
+++ Slider/app.js
@@ -0,0 +1,42 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach((item, index) => {
+  item.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  counter--;
+  carousel();
+});
+
+const carousel = () => {
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+  if (counter > slides.length - 1) {
+    counter = 0;
+  }
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter}00%)`;
+  });
+
+  // if (counter < slides.length - 1) {
+  //   nextBtn.style.display = 'block';
+  // } else {
+  //   nextBtn.style.display = 'none';
+  // }
+  // if (counter > 0) {
+  //   prevBtn.style.display = 'block';
+  // } else {
+  //   prevBtn.style.display = 'none';
+  // }
+};
